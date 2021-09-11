0d63266d24613bf582c7802dfd5daed6b61d7bf6
LAPTOP-U61E0LQE\Kenneth <kenneth_oafallas@dlsu.edu.ph>
2021-09-11 19:46:16 +0800

    mom sprite and cursor

--- Assets/Scripts/ShowCursor.cs
+++ Assets/Scripts/ShowCursor.cs
@@ -4,20 +4,21 @@ using System.Collections;
 public class ShowCursor : MonoBehaviour
 {
     public Texture2D cursorTexture;
-    public CursorMode cursorMode = CursorMode.Auto;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
     public Vector2 hotSpot = Vector2.zero;
     void Start() 
     {
         Cursor.visible = true;
         Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
     }
-    void OnMouseEnter()
-    {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
-    }
+    
+    // void OnMouseEnter()
+    // {
+    //     Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    // }
 
-    void OnMouseExit()
-    {
-        Cursor.SetCursor(null, Vector2.zero, cursorMode);
-    }
+    // void OnMouseExit()
+    // {
+    //     Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    // }
 }
